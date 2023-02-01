e3625b0d9bd5ea4b289629cdb15b64794532623f
Tory DeMaio <torydemaio@gmail.com>
2023-02-01 16:32:10 -0600

    corrected import

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { Container,Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './app/assets/img/logo.png';
 
 function App() {
   return (
     <div className="App">
-      Hello nucamp!
+      <Navbar dark color='primaty' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={NucampLogo} alt='nucamp logo'></img>
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      I'm ready for workshop!
     </div>
   );
 }
