1a0bd0a27e7a1d0768de5c123b2ea7244babf4bc
Tory DeMaio <torydemaio@gmail.com>
2023-02-06 16:08:53 -0600

    CampsitesDirectoryDirectoryPage and CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import CampsitesList from './features/campsites/CampsitesList';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer/>
     </div>
   );
---
+++ src/features/campsites/CampsiteDetail.js
@@ -0,0 +1,20 @@
+import { Card, CardImg, CardBody, Col, CardText } from "reactstrap";
+
+const CampsiteDetail = ({campsite})=>{
+    const {image, name, description} = campsite;
+
+    return(
+        <Col md='12' className="m-4">
+            <Card>
+                <CardImg top src={image} alt={name}/>
+                <CardBody>
+                    <CardText>
+                        {description}
+                    </CardText>
+                </CardBody>
+            </Card>
+        </Col>
+    )
+}
+
+export default CampsiteDetail
