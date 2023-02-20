12d99cbefbd8e11a72c942a35427b812c184ff8b
Tory DeMaio <torydemaio@gmail.com>
2023-02-20 15:34:45 -0600

    ContactPage

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,33 @@
+import { Col, Container, Row } from "reactstrap";
+import SubHeader from "../components/SubHeader";
+
 const ContactPage =() => {
-    return <div>ContactPage</div>
+    return (
+        <Container>
+            <SubHeader current='Contact Us' />
+            <Row className="row-content align-items-center">
+                <Col sm='4'>
+                    <h5>Our Address</h5>
+                    <address>1 Nucamp Way <br/> Seattle,
+                     WA 98001 <br/>
+                    U.S.A.</address>
+                </Col>
+                <Col>
+                    <a role='button' className="btn btn-link" href="tel:+1-206-555-1234"><i className="fa fa-phone"> 1-206-555-1234</i></a>
+                    <br/>
+                    <a role='button' className="btn btn-link" href="mailto:fakeemail@fakeemal.co"><i className="fa fa-envelope"> campsites@nucamp.co</i></a>
+                </Col>
+            </Row>
+            <Row className="row-content">
+                <Col xs='12'>
+                    <h2>Send Us Your Feedback</h2>
+                    <hr/>
+                </Col>
+                <Col md='10'>TBD: ContactForm
+                </Col>
+            </Row>
+        </Container>
+    )
 };
 
 export default ContactPage;
