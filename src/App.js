55e119805653ee983a50df1067b163779f4d82d1
Tory DeMaio <torydemaio@gmail.com>
2023-03-05 22:06:44 -0600

    Async Promotions

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
 import { fetchPartners } from './features/partners/partnersSlice';
+import { fetchPromotions } from './features/promotions/promotionsSlice';
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchCampsites());
-    dispatch(fetchPartners())
+    dispatch(fetchPartners());
+    dispatch(fetchPromotions());
 }, [dispatch]);
 
   return (
